798e2bffd38ee4f7bc45cc7c21b39236054af10b
Asif Mulla <asifmulla1520031@gmail.com>
2025-07-30 17:13:18 +0530

    changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import ProjectDetails from "./ProjectDetails";
 
 const Project = ({ title, description, subDescription, href, image, tags }) => {
